3651b066eb4279eda1e4e618a99d66f91780e3d3
mulumpyo <1119_dh@naver.com>
2025-11-13 23:08:52 +0900

    배포 수정

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,8 @@
-import { fetchApi } from "@/lib/api";
-
-export const revalidate = 0;
-
 const Home = async () => {
-  const server = await fetchApi("/api/test");
 
   return (
     <div>
       <h2 className="text-2xl font-semibold mb-4">서버 렌더링 (SSR)</h2>
-      <div className="mt-4 p-4 border rounded bg-white">
-        <p>{server?.serverTime}</p>
-      </div>
     </div>
   );
 };
