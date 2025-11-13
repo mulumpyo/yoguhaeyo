0d4c467585c426631d4309cbad1074d26ecb40cb
mulumpyo <1119_dh@naver.com>
2025-11-13 22:51:05 +0900

    깃허브 액션 수정.

--- app/app/page.jsx
+++ app/app/page.jsx
@@ -1,14 +1,24 @@
-"use client"; 
+"use client";
 
 import { useEffect, useState } from "react";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import axios from "axios";
 
 const ClientPage = () => {
   const [count, setCount] = useState(0);
-  const [time, setTime] = useState("");
+  const [serverTime, setServerTime] = useState("");
 
   useEffect(() => {
-    setTime(new Date().toLocaleTimeString());
+    const fetchServerTime = async () => {
+      try {
+        const res = await axios.get("/api/test");
+        setServerTime(res.data.serverTime);
+      } catch (err) {
+        console.error("Failed to fetch server time:", err);
+      }
+    };
+
+    fetchServerTime();
   }, [count]);
 
   return (
@@ -16,7 +26,7 @@ const ClientPage = () => {
       <h2 className="text-2xl font-semibold mb-4">CSR</h2>
 
       <div className="mt-4 flex flex-col items-start gap-2">
-        <p className="text-gray-500">현재 시간: {time}</p>
+        <p className="text-gray-700">DB 서버 시간: {serverTime}</p>
         <Button onClick={() => setCount(count + 1)}>{count}</Button>
       </div>
     </div>
--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,15 @@
-import { API_URL } from "../lib/config";
+import { fetchApi } from "@/lib/api";
+
+export const revalidate = 0;
 
 const Home = async () => {
-
-  const data = await fetch(`${API_URL}/api/test`);
-
-  const server = await data.json();
+  const server = await fetchApi("/api/test");
 
   return (
     <div>
       <h2 className="text-2xl font-semibold mb-4">서버 렌더링 (SSR)</h2>
-
       <div className="mt-4 p-4 border rounded bg-white">
-        <p>{server.serverTime}</p>
+        <p>{server?.serverTime}</p>
       </div>
     </div>
   );
