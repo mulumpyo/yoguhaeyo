c140eaf60ea05fe7f4a7e79ba8c3abfebf8dd692
mulumpyo <1119_dh@naver.com>
2025-11-18 13:07:52 +0900

    토큰 없이 api 요청시 재발급 시도4

--- components/lib/api.js
+++ components/lib/api.js
@@ -6,64 +6,13 @@ const api = axios.create({
   withCredentials: true,
 });
 
-let isRefreshing = false;
-let failedQueue = [];
-
-const processQueue = (error, token = null) => {
-  failedQueue.forEach((prom) => {
-    if (error) {
-      prom.reject(error);
-    } else {
-      prom.resolve(token);
-    }
-  });
-  failedQueue = [];
-};
-
 api.interceptors.response.use(
   (response) => response,
   async (error) => {
     const status = error?.response?.status;
-    const originalRequest = error.config;
 
-    if (status === 401 && !originalRequest._retry) {
-
-      if (isRefreshing) {
-        return new Promise((resolve, reject) => {
-          failedQueue.push({ resolve, reject });
-        })
-          .then(() => {
-            return api({
-              ...originalRequest,
-              withCredentials: true,
-            });
-          })
-          .catch((err) => Promise.reject(err));
-      }
-
-      originalRequest._retry = true;
-      isRefreshing = true;
-
-      try {
-        const { data } = await axios.post(
-          `${process.env.BASE_URL}/api/auth/refresh`,
-          {},
-          { withCredentials: true }
-        );
-
-        processQueue(null, true);
-
-        return api({
-          ...originalRequest,
-          withCredentials: true,
-        });
-      } catch (refreshError) {
-        processQueue(refreshError, null);
-        await logout();
-        return Promise.reject(refreshError);
-      } finally {
-        isRefreshing = false;
-      }
+    if (status === 401) {
+      await logout();
     }
 
     return Promise.reject(error);
