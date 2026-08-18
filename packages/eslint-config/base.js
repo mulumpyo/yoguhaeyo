4c491473cc04d71e22c441201fa544c5f12a5c25
mulumpyo <1119_dh@naver.com>
2026-08-19 01:20:40 +0900

    Turborepo 구축 (Nuxt + NestJS)

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -27,6 +27,11 @@ export const config = [
     },
   },
   {
-    ignores: ["dist/**"],
+    ignores: [
+      "dist/**", 
+      ".nuxt/**",
+      ".output/**",
+      "node_modules/**"
+    ],
   },
 ];
